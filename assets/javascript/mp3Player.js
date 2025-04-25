fd62ab8f10c320ed91ea53a88dbb245408a18bed
atlas <atlasstrummermorgan@gmail.com>
2025-04-24 20:24:36 -0400

    Add in media player

---
+++ assets/javascript/mp3Player.js
@@ -0,0 +1,28 @@
+const audio = document.getElementById('audio-player');
+const playBtn = document.getElementById('play-btn');
+const pauseBtn = document.getElementById('pause-btn');
+const stopBtn = document.getElementById('stop-btn');
+const seekBar = document.getElementById('seek-bar');
+
+playBtn.addEventListener('click', () => {
+  audio.play();
+});
+
+pauseBtn.addEventListener('click', () => {
+  audio.pause();
+});
+
+stopBtn.addEventListener('click', () => {
+  audio.pause();
+  audio.currentTime = 0;
+});
+
+audio.addEventListener('timeupdate', () => {
+  const value = (audio.currentTime / audio.duration) * 100;
+  seekBar.value = value || 0;
+});
+
+seekBar.addEventListener('input', () => {
+  const time = (seekBar.value / 100) * audio.duration;
+  audio.currentTime = time;
+});>
